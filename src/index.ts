65630857d11ceadfbb0917d0e1b17ef82702cf59
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-02-20 17:47:30 -0600

    move schema to assets/schema.graphql

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { ItemDataSource } from "./datasources/item";
 import { TaleDataSource } from "./datasources/tale";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { readFileSync } from "fs";
+import path from "path";
 import { resolvers } from "./resolvers";
 import { GOIDataSource } from "./datasources/goi";
 
 const startApolloServer = async () => {
+  const typeDefs = readFileSync(path.resolve(__dirname, "../assets/schema.graphql"), { 
+    encoding: "utf-8" 
+  });
   const server = new ApolloServer({
     typeDefs,
     resolvers,
